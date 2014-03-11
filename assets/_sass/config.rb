1a22965d3627104fc498e0211375fb00b8f955fc
Bud Parr <budparr@sonnetmedia.net>
2014-03-11 09:23:12 -0400

    update css file so that it is compressed

--- assets/_sass/config.rb
+++ assets/_sass/config.rb
@@ -21,7 +21,7 @@ relative_assets = true
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 # Over-ride with force compile to change output style with: compass compile --output-style compressed --force
-output_style = :expanded
+output_style = :compressed
 
 line_comments = false
 cache = true

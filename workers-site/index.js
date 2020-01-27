03f80b1607bb9ecfe8742e838b7ccda26654c546
budparr <budparr@gmail.com>
2020-01-27 16:02:29 -0500

    Modify default Workers Site script to use security headers and local KV-assets

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,28 +1,9 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-let securityHeaders = {
-    "Content-Security-Policy": "upgrade-insecure-requests",
-    "Strict-Transport-Security": "max-age=2592000",
-    "X-Xss-Protection": "1; mode=block",
-    "X-Frame-Options": "DENY",
-    "X-Content-Type-Options": "nosniff",
-    "Referrer-Policy": "strict-origin-when-cross-origin",
-    "Feature-Policy": "camera 'none'; geolocation 'none'; microphone 'none'"
-};
+// import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV } from "./kv-asset-handler.ts";
+import { SetHeaders } from "./security-headers";
 
-let sanitiseHeaders = {
-    Server: "My New Server Header!!!"
-};
 
-let removeHeaders = ["Public-Key-Pins", "X-Powered-By", "X-AspNet-Version"];
-// let cacheHeaders = {
-//     "Cache-Control": "public, max-age=1, must-revalidate",
-// };
-let cacheControl = {
-    //Cloudflare Cache Settings
-    browserTTL: null, // do not set cache control ttl on responses
-    edgeTTL: 2 * 60 * 60 * 24, // 2 days
-    bypassCache: false // do not bypass Cloudflare's cache
-};
+
 /**
  * The DEBUG flag will do two things that help during development:
  * 1. we will skip caching on the edge, which makes it easier to
@@ -30,101 +11,74 @@ let cacheControl = {
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
-const DEBUG = false;
+const DEBUG = false
 
-addEventListener("fetch", event => {
-    try {
-        event.respondWith(handleEvent(event).then(SetHeaders));
-    } catch (e) {
-        if (DEBUG) {
-            return event.respondWith(
-                new Response(e.message || e.toString(), {
-                    status: 500
-                })
-            );
-        }
-        event.respondWith(new Response("Internal Error", { status: 500 }));
+addEventListener('fetch', event => {
+  try {
+    event.respondWith(handleEvent(event).then(SetHeaders))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
     }
-});
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
+})
 
 async function handleEvent(event) {
-    const url = new URL(event.request.url);
-    let options = {};
+  const url = new URL(event.request.url)
+  let options = {}
 
-    // let cache  = {
-    //     etag: '0928409987487333'
-    // }
+  /**
+   * You can add custom logic to how we fetch your assets
+   * by configuring the function `mapRequestToAsset`
+   */
+  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
 
-    options.cacheControl = cacheControl;
-    try {
-        if (DEBUG) {
-            // customize caching
-            options.cacheControl = {
-                bypassCache: true
-            };
-        }
-        return await getAssetFromKV(event, options);
-    } catch (e) {
-        // if an error is thrown try to serve the asset at 404.html
-        if (!DEBUG) {
-            try {
-                let notFoundResponse = await getAssetFromKV(event, {
-                    mapRequestToAsset: req =>
-                        new Request(`${new URL(req.url).origin}/404.html`, req)
-                });
-
-                return new Response(notFoundResponse.body, {
-                    ...notFoundResponse,
-                    status: 404
-                });
-            } catch (e) {}
-        }
-
-        return new Response(e.message || e.toString(), { status: 500 });
+  try {
+    if (DEBUG) {
+      // customize caching
+      options.cacheControl = {
+        bypassCache: true,
+      }
     }
+    return await getAssetFromKV(event, options)
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
+        })
+
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+      } catch (e) {}
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 })
+  }
 }
 
-const SetHeaders = function(response) {
-        response = new Response(response.body, response);
+/**
+ * Here's one example of how to modify a request to
+ * remove a specific prefix, in this case `/docs` from
+ * the url. This can be useful if you are deploying to a
+ * route on a zone, or if you only want your static content
+ * to exist at a specific path.
+ */
+function handlePrefix(prefix) {
+  return request => {
+    // compute the default (e.g. / -> index.html)
+    let defaultAssetKey = mapRequestToAsset(request)
+    let url = new URL(defaultAssetKey.url)
 
-        let newHdrs = new Headers(response.headers);
+    // strip the prefix from the path for lookup
+    url.pathname = url.pathname.replace(prefix, '/')
 
-        //let cache = caches.default;
-        var ETag = response.headers.get('ETag');
-
-        if (
-            newHdrs.has("Content-Type") &&
-            !newHdrs.get("Content-Type").includes("text/html")
-        ) {
-            return new Response(response.body, {
-                status: response.status,
-                statusText: response.statusText,
-                headers: newHdrs
-            });
-        }
-
-        let setHeaders = Object.assign(
-            {},
-            securityHeaders,
-            sanitiseHeaders,
-            ETag
-            // ,
-            // cacheHeaders
-        );
-
-        Object.keys(setHeaders).forEach(name => {
-            // https://scotthelme.co.uk/security-headers-cloudflare-worker/
-            // use append instead of set to respect previously set headers
-            newHdrs.append(name, setHeaders[name]);
-        });
-
-        removeHeaders.forEach(name => {
-            newHdrs.delete(name);
-        });
-
-        return new Response(response.body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: newHdrs
-        });
-    };
+    // inherit all other props from the default request
+    return new Request(url.toString(), defaultAssetKey)
+  }
+}

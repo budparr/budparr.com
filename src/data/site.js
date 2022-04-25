e1e00b18a0e44959e85228ab146f4c1c3dcddafd
Bud Parr <budparr@thenewdynamic.com>
2022-04-25 13:53:14 -0400

    Improve handling of Alpine/js

--- src/data/site.js
+++ src/data/site.js
@@ -3,11 +3,7 @@ export default {
     description: `Personal Website of Bud Parr`,
     url: `https://www.budparr.com`, // No trailing slash!
     // JSON LD
-    name: `budparr.com`,
-    // Facebook URL
-    facebookURL: `https://facebook.com`,
+    name: `budparr`,
     // Twitter URL
     twitterURL: `https://twitter.com`,
-    // Instagram URL
-    instagramURL: `https://instagram.com/`,
 }
